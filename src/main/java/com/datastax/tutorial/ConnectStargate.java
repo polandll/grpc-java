3f66ed1f7e8c750f4f35970af42cdf8e4aa13691
Lorina Poland <lorinapoland@gmail.com>
2021-11-09 12:36:34 -0800

    change names

--- src/main/java/com/datastax/tutorial/ConnectStargate.java
+++ src/main/java/com/datastax/tutorial/ConnectStargate.java
@@ -71,11 +71,11 @@ public class ConnectStargate {
                 QueryOuterClass.Batch.newBuilder()
                     .addQueries(
                         QueryOuterClass.BatchQuery.newBuilder()
-                            .setCql("INSERT INTO test.users (firstname, lastname) VALUES('Lorina', 'Poland')")
+                            .setCql("INSERT INTO test.users (firstname, lastname) VALUES('Jane', 'Doe')")
                             .build())
                     .addQueries(
                         QueryOuterClass.BatchQuery.newBuilder()
-                            .setCql("INSERT INTO test.users (firstname, lastname) VALUES('Doug', 'Wettlaufer')")
+                            .setCql("INSERT INTO test.users (firstname, lastname) VALUES('Serge', 'Provencio')")
                             .build())
                     .build());
         System.out.println("2 rows have been inserted in table users.");
